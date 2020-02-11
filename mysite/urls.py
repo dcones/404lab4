601ee81a3b1e410d45c75ba28934c6a3e5b3df24
Daniel Cones <dcones@ualberta.ca>
2020-02-10 17:21:09 -0700

    heroku

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
 
 urlpatterns = [
+  path('', lambda x: HttpResponse("Hello, world. You're at the polls index."), name='index'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
